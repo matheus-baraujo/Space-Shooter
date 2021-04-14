5910d0c7bffda698a3df3bcefe514b46f85e3e27
MBA <matheus-araujo1998@hotmail.com>
2021-04-14 14:46:32 -0300

    movimento e rotaçao do player

--- classes.py
+++ classes.py
@@ -1,3 +1,6 @@
+import pygame
+import sys
+
 class PlayerObject():
 
 
@@ -8,21 +11,32 @@ class PlayerObject():
         initial_vertex3 = [width/2 + 10,height/2 + 20]
         self.vertices = [initial_vertex1, initial_vertex2, initial_vertex3]
         self.life = 5
+        
+        self.surface = pygame.image.load() #adicionar uma imagem pra ser o player
+        self.rotated_surface = self.surface.copy()
+        self.directions = [False, False, False, False] # UP, DOWN, RIGHT, LEFT
+        self.speed = 0.1
 
     def is_dead(self):
 
         if self.life < 1:
-
             return False
-        
         else:
-
             return True
 
+    def moves(self):
+        if self.directions[0] || self.directions[1] or self.directions[2] || self.directions[3]:
+            self.y += (self.directions[0] - self.directions[1])*self.speed
+            self.x += (self.directions[2] - self.directions[3])*self.speed
+            
+    def rotate(self, angle):
+        rot_image = pygame.transform.rotozoom(self.surface, angle, 1)
+        rot_rect = self.rect.copy()
+        rot_rect.center = rot_image.get_rect().center
+        self.rotated_surface = rot_image        
+        
     def get_vertices(self):
-
         return self.vertices
 
     def update_vertices(self, new_vertices):
-
         self.vertices = new_vertices
--- main.py
+++ main.py
@@ -17,7 +17,36 @@ def main():
 
         for event in pygame.event.get():
             
-            if event.type==pygame.QUIT: sys.exit()
+            if event.type==pygame.QUIT: 
+                sys.exit()
+
+            elif event.type == pygame.KEYDOWN: #buttons pressed
+                if event.key == pygame.K_ESCAPE:
+                    sys.exit()
+                if event.key == pygame.K_w:
+                    player.directions[0] = 1
+                if event.key == pygame.K_s:
+                    player.directions[1] = 1
+                if event.key == pygame.K_a:
+                    player.directions[2] = 1
+                if event.key == pygame.K_d:
+                    player.directions[3] = 1
+
+            elif event.type == pygame.KEYUP: #buttons not pressed
+                if event.key == pygame.K_w:
+                    player.directions[0] = 0
+                if event.key == pygame.K_s:
+                    player.directions[1] = 0
+                if event.key == pygame.K_a:
+                    player.directions[2] = 0
+                if event.key == pygame.K_d:
+                    player.directions[3] = 0
+            elif event.type == pygame.MOUSEMOTION:
+                mouse_x = pygame.mouse.get_pos()[0]
+                mouse_y = pygame.mouse.get_pos()[1]
+                angle = math.atan2(mouse_y - player.y, mouse_x - player.x)
+                angle = angle * (180 / math.pi)
+                player.rotate(angle)
 
         player_vertices = player.get_vertices()
         screen.fill(BLACK)
