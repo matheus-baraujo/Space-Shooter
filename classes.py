114ff44bb4bfc883d7e90adb67e27f91c7c6cffd
MBA <matheus-araujo1998@hotmail.com>
2021-04-15 13:05:27 -0300

    corrected displacement error

--- classes.py
+++ classes.py
@@ -72,11 +72,11 @@ class Spacecraft():
         
         self.x += displacement_x
         self.y += displacement_y
-        for vertex in self.vertices:
+        '''for vertex in self.vertices:
             vertex[0] += displacement_x
             vertex[1] += displacement_y
             vertex[0] %= WIDTH
-            vertex[0] %= HEIGHT
+            vertex[0] %= HEIGHT'''
             
     def rotate_spacecraft(self, angular_displacement):
 
@@ -114,8 +114,8 @@ class Player(Spacecraft):
     def move(self):
 
         if self.directions[0] or self.directions[1] or self.directions[2] or self.directions[3]:
-            displacement_y += (self.directions[0] - self.directions[1])*self.speed
-            displacement_x += (self.directions[2] - self.directions[3])*self.speed
+            displacement_y = (self.directions[0] - self.directions[1])*self.speed
+            displacement_x = (self.directions[2] - self.directions[3])*self.speed
             self.translate_spacecraft(displacement_x, displacement_y)
         
             
