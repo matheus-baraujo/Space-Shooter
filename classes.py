cd9c02b423dafb8c1a056d561b7f016cbb63892e
Marcus <marcusvmelodev@gmail.com>
2021-04-22 11:45:02 -0300

    Merged classes.py and projectile.py files and put player into Sprite class

--- classes.py
+++ classes.py
@@ -114,6 +114,10 @@ class Spacecraft():
         direction = tuple(x*math.sqrt(direction[0]**2 + direction[1]**2) for x in direction)
         return Projectile(self.vertices[0][0], self.vertices[0][1], speed, color, PROJECTILE_RADIUS, direction)
 
+    def get_position(self):
+
+        return self.x, self.y
+
 
 class Player(Spacecraft):
 
@@ -208,4 +212,65 @@ class Enemy(Spacecraft):
 
     def update(self, x_player, y_player):
 
+        self.move(x_player, y_player)
         self.rotate(x_player, y_player)
+
+
+class Projectile():
+
+    def __init__(self, x_position, y_position, speed, color, radius, direction):
+
+        self.x = x_position
+        self.y = y_position
+        self.speed = speed
+        self.color = color
+        self.radius = radius 
+        self.direction = direction
+
+    def draw(self, screen):
+
+        pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius)
+
+    def move(self):
+
+        self.x += self.direction[0] * self.speed  
+        self.y += self.direction[1] * self.speed
+
+    def is_out_of_bound(self):
+
+        if self.x<0 or self.x>WIDTH or self.y<0 or self.y>HEIGHT:
+            return True
+        else:
+            return False
+
+
+class Sprites():
+
+
+    def __init__(self player):
+
+        self.projectiles = []
+        self.enemeies = []
+        self.player = player
+
+    def add_projectile(self, projectile):
+
+        self.projectiles.append(projectile)
+
+    def add_enemy(self, enemy):
+
+        self.enemeies.append(enemy)
+
+    def update(self):
+
+        for projectile in self.projectiles:
+            projectile.move()
+            if projectile.is_out_of_bound():
+                self.projectiles.remove(projectile)
+                del projectile            
+
+    def draw(self, screen):
+
+        for projectile in self.projectiles:
+
+            projectile.draw(screen)
