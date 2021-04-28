96b407e4da9cbf4fdf1ad6aa04e338787a01cf70
Marcus <marcusvmelodev@gmail.com>
2021-04-28 01:52:56 -0300

    made enemies aggressive

--- functions.py
+++ functions.py
@@ -5,7 +5,7 @@ import random
 
 def create_enemy(score):
 
-    level = min((5,score/2500))
+    level = int(score/2500)
     level = max((1, level))
     colors = PURPLE, ORANGE, CYAN, BLUE
     shapes = shape1,
@@ -14,8 +14,8 @@ def create_enemy(score):
     x_position = random.randrange(10, WIDTH-10)
     y_position = random.randrange(10, HEIGHT-10)
     life = random.randint(1, int(level))
-    speed = random.uniform(0.01, 0.1) * level
-    angle_speed = random.uniform(0.01, 0.02) * level
+    speed = random.uniform(0.1, 0.2) * level
+    angle_speed = random.uniform(0.1, 0.2) * level
     max_distance = random.randint(150, 400)
     enemy = classes.Enemy(x_position, y_position, life, speed, shape, color, angle_speed, 0, max_distance)
     return enemy
